98efa6457b91142e0ccae3f91c1fa1ef8067360f
naimuddin94 <developernaimuddin@gmail.com>
2025-03-30 22:22:06 +0600

    server url change

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,2 +1,2 @@
-export const baseUrl = "https://font-group-system-ujy5.onrender.com";
+export const baseUrl = "https://font-group-system-65t1.onrender.com";
 // export const baseUrl = "http://localhost:5000";
